0506086d7935ccbcc56d95304bd4a01a935c83c9
DiegoGustavoMO <71155173+DiegoGustavoMO@users.noreply.github.com>
2021-11-08 20:08:32 -0500

    Inicio programa de ejemplo en flutter

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+//siempre hay que importar la libreria de material para flutter
+import 'package:flutter/material.dart';
+
+//la clase main principal que ejecuta la aplicacion al inciar es
+//en este casso MyApp debe tener la primera letra en mayuscula
+void main() => runApp(MyApp());
+//con este main estoy indicando que ejecute la aplicacion principal
+
+//con la ayuda del los pluggins usamos statlessW o StatefulW
+//la diferencia entre estos es que en el full podemos sobre escribir
+
+//se crea el primer widget (clase) de la aplicacion
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  //este es un contructor (build) donde se construye el widget y que retorna
+  //un widget
+
+  @override
+  Widget build(BuildContext context) {
+    //devolvemos un MaterialApp , Material es un patron de diseño de google
+    //material tiene ciertos parametros como title,home
+    //title define el titulo de la aplicacion
+    //home lo que aparece en el inicio de la aplicacion
+    return MaterialApp(
+      //Para quitar el banner de debug
+      debugShowCheckedModeBanner: false,
+      title: "Mi App",
+      home: Inicio(),
+    );
+  }
+}
+
+//como en home colocamos que llame a un widget (clase) llamado Inicio()
+//debemos crearlo
+
+//creamos el widget Inicio() de tipo stateful
+class Inicio extends StatefulWidget {
+  Inicio({Key? key}) : super(key: key);
+
+  @override
+  _InicioState createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    //debemos retornar un widget usado en aplicaciones tradicionales
+    //llamado Scaffold()
+    //debemos recordar que hay widget que son contenedores de otros widgets
+    //por eso se ve como si fuera una clase dentro de otra clase
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Mi App"),
+        ),
+        body: Center(
+          child: Text("Contenido"),
+        ));
+  }
+}
